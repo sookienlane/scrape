c860f9483462996b26d2c9cb527f2e1c8e720728
sookienlane <yang2090893@gmail.com>
2019-10-09 20:16:17 +0800

    add base64 hash

--- requests_lxml_scrape_jandan/main.py
+++ requests_lxml_scrape_jandan/main.py
@@ -7,10 +7,20 @@
 import requests
 from lxml import etree
 import numpy as np
+import base64
+import datetime
 
-number = 8
+number = 1
 thres = 100
-res = requests.get("https://jandan.net/ooxx/page-{}#comments".format(number))
+
+res = requests.get("https://jandan.net/ooxx")
+page = etree.HTML(res.text)
+current_page = page.xpath("//span[@class='current-comment-page']/text()")[0].replace("[","").replace("]","")
+print(current_page)
+
+hash_time = base64.b64encode((datetime.datetime.now().strftime("%Y%m%d")+"-"+str(number)).encode("utf-8")).decode("utf-8")
+url = "https://jandan.net/ooxx/{}#comments".format(hash_time)
+res = requests.get(url)
 page = etree.HTML(res.text)
 img_list = page.xpath(
     '//ol[@class="commentlist"]/li/div[1]/div/div[@class="text"]/p[1]/a[1]/@href')
@@ -18,4 +28,4 @@ oo_list = page.xpath(
     '//ol[@class="commentlist"]/li/div/div/div[@class="jandan-vote"]/span[@class="tucao-like-container"]/span/text()')
 
 img_index = [int(x) > thres for x in oo_list]
-np.array(img_list)[img_index].tolist()
+["http:"+x for x in np.array(img_list)[img_index].tolist()]
